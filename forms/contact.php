ebcbb7163a73c1aabbcab9170015434b6908a11f
Miguel Esparza <42750822+miesparzaDelee@users.noreply.github.com>
2023-01-18 08:40:04 -0600

    de

--- forms/contact.php
+++ forms/contact.php
@@ -13,7 +13,8 @@ $to = "ventas@texastransportf.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From:". $email;
-echo "Gets here $header";
-if(!mail($to, $subject, $body, $header))
+if(!mail($to, $subject, $body, $header)){
   http_response_code(500);
+}
+echo "OK";
 ?>
