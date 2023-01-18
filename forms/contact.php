0bad9cd01fc8a39f68575793cc1a27b75f2c41d5
Miguel Esparza <42750822+miesparzaDelee@users.noreply.github.com>
2023-01-18 09:34:17 -0600

    update

--- forms/contact.php
+++ forms/contact.php
@@ -11,7 +11,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 
 $to = "ventas@texastransportf.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$body = "Has recibido un mensaje nuevo de la página en la sección contacto.\n\n"."Aqui estan los detalles:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMensaje: $message";
 $header = "From:". $email;
 if(!mail($to, $subject, $body, $header)){
   http_response_code(500);
